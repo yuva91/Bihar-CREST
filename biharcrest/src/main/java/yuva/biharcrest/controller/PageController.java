c93bd0aefbc645b742aaa5b6e86b894b061f58c5
Yuvaraja <yuvarajiskrishnappa@gmail.com>
2018-12-27 01:51:37 +0530

    Day1

---
+++ biharcrest/src/main/java/yuva/biharcrest/controller/PageController.java
@@ -0,0 +1,18 @@
+package yuva.biharcrest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting","Welcome to spring web MVC");
+		return mv; 
+	}
+
+}
